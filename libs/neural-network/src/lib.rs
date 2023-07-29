707be5aedf5128ac318f3427b38aba0c67b3d9fd
Red Rapious <red.rapious@gmail.com>
2023-07-29 15:39:45 +0200

    Finished Part 2: network init and tests

--- libs/neural-network/src/lib.rs
+++ libs/neural-network/src/lib.rs
@@ -1,3 +1,5 @@
+use rand::Rng;
+
 pub struct Network {
     layers: Vec<Layer>,
 }
@@ -7,6 +9,20 @@ impl Network {
         self.layers
             .iter()
             .fold(inputs, |inputs, layer| layer.propagate(inputs))
+    }
+
+    pub fn random(layers: &[LayerTopology]) -> Self {
+        assert!(layers.len() > 1);
+
+        let layers = layers
+            .windows(2)
+            .map(|layers| {
+                Layer::random(layers[0].neurons, layers[1].neurons)
+            })
+            .collect();
+
+
+        Self { layers }
     }
 } 
 
@@ -20,6 +36,14 @@ impl Layer {
             .iter()
             .map(|neuron| neuron.propagate(&inputs))
             .collect()
+    }
+
+    pub fn random(input_neurons: usize, output_neurons: usize) -> Self {
+        let neurons = (0..output_neurons)
+            .map(|_| Neuron::random(&mut rand::thread_rng(), input_neurons))
+            .collect();
+
+        Self { neurons }
     }
 }
 
@@ -40,4 +64,69 @@ impl Neuron {
 
         (output + self.bias).max(0.0)
     }
+
+    pub fn random(rng: &mut dyn rand::RngCore, output_size: usize) -> Self {
+        let bias = rng.gen_range(-1.0..=1.0);
+
+        let weights = (0..output_size)
+            .map(|_| rng.gen_range(-1.0..=1.0))
+            .collect();
+
+        Self { bias, weights }
+    }
 } 
+
+pub struct LayerTopology {
+    pub neurons: usize
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    mod random {
+        use super::*;
+        use rand::SeedableRng;
+        use rand_chacha::ChaCha8Rng;
+
+        #[test]
+        fn test() {
+            let mut rng = ChaCha8Rng::from_seed(Default::default());
+            let neuron = Neuron::random(&mut rng, 4);
+
+            approx::assert_relative_eq!(
+                neuron.bias, 
+                -0.6255188
+            );
+            approx::assert_relative_eq!(
+                neuron.weights
+                    .as_slice(), 
+                [0.67383957, 0.8181262, 0.26284897, 0.5238807]
+                    .as_ref()
+            );
+        }
+    }
+
+    mod propagate {
+        use super::*;
+
+        #[test]
+        fn test() {
+            let neuron = Neuron {
+                bias: 0.5,
+                weights: vec![-0.3, 0.8]
+            };
+
+            // Makes sure that ReLU works
+            approx::assert_relative_eq!(
+                neuron.propagate(&[-10.0, -10.0]), 
+                0.0
+            );
+
+            approx::assert_relative_eq!(
+                neuron.propagate(&[0.5, 1.0]),
+                (-0.3 * 0.5) + (0.8 * 1.0) + 0.5
+            );
+        }
+    }
+}
