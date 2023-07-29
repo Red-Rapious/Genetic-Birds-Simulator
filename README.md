5ec0e8ab857d6d23032b12a73a7ddfcc2c620579
Red Rapious <red.rapious@gmail.com>
2023-07-29 13:55:18 +0200

    Workspace initialisation and propagate functions

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Genetic-Birds-Simulator
 A simulation of birds using neural networks and a genetic algorithm.
+
+## About
+### Motivation
+This project follows [MLP-Digits-Recognition](), an implementation of a neural network from scratch. This time, I used [this great tutorial](https://pwy.io/posts/learning-to-fly-pt1/) as a starting point, to train the neural network with a genetic algorithm, instead of backpropagation.
---
+++ libs/neural-network/src/lib.rs
@@ -0,0 +1,43 @@
+pub struct Network {
+    layers: Vec<Layer>,
+}
+
+impl Network {
+    pub fn propagate(&self, inputs: Vec<f32>) -> Vec<f32> {
+        self.layers
+            .iter()
+            .fold(inputs, |inputs, layer| layer.propagate(inputs))
+    }
+} 
+
+struct Layer {
+    neurons: Vec<Neuron>,
+}
+
+impl Layer {
+    fn propagate(&self, inputs: Vec<f32>) -> Vec<f32> {
+        self.neurons
+            .iter()
+            .map(|neuron| neuron.propagate(&inputs))
+            .collect()
+    }
+}
+
+struct Neuron {
+    bias: f32,
+    weights: Vec<f32>,
+}
+
+impl Neuron {
+    fn propagate(&self, inputs: &[f32]) -> f32 {
+        assert_eq!(inputs.len(), self.weights.len());
+
+        let output = inputs
+            .iter()
+            .zip(&self.weights)
+            .map(|(input, weight)| input * weight)
+            .sum::<f32>();
+
+        (output + self.bias).max(0.0)
+    }
+} 
